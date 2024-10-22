352bbe81bd53b4dc4f20187e866ba1c2052cf7bf
Elmer <elmer.tolentinohansson2@elev.ga.ntig.se>
2024-10-22 10:14:54 +0200

    la till några fixish på hashtaggen

---
+++ skibidiween.py
@@ -0,0 +1,14 @@
+# halloween.py
+
+# Först frågar vi användaren om deras favorit Skibidiween karaktär
+karaktar = input("Vilken är din favorit skibidiween-karaktär? (skibidi toilet, tv woman, skibidiflash): ").lower()
+
+# Sen använder vi en if-sats för att ge ett svar baserat på användarens val
+if karaktar == "skibidi toilet":
+    print("SKIBIDI DOP DOP DOP YES YES!")
+elif karaktar == "tv woman":
+    print("baddieee... baddieee...")
+elif karaktar == "skibidiflash":
+    print("Wooo Big BBC not the little one Yeah Fuck Yeah BBC Flash Not the little one Remember that nigga Hon frågar hur jag mår Jag sa bae jag mår fantastiskt Hon rider på min kuk Och hon rider som gymnastics Vi ska inte gifta oss Vill ha dig för en nattish Hon vill ha den var som Så jag gav henne den kraftigt Hon har hela munnen öppen när hon suger på min snopp Jag har tjänat snart en mille Jag har sålt så mycket kox Din guzz våldtar *?* Kanske därför hon blivit tjock Hon vill suga på min kuk För hon såg mig på TikTok Hon frågar hur jag mår Jag sa bitch jag mår amazing Guzzarna dom älskar för jag köper allt dom glazing *?*, vill inte ha någon baby Jag var horny som fan Så jag doggade en främling Huh Hon skriker och stör mina grannar De min sång bre jag pussar din panna Jag är horny, du kommer med grabbar Inga *?* jag kommer med tabbar! Hon frågar hur jag mår Jag sa bae jag mår fantastiskt Hon rider på min kuk Och hon rider som gymnastics Vi ska intе gifta oss Vill ha dig för en nattish Hon vill ha den var som Så jag gav hennе den kraftigt")
+else:
+    print("L karaktär")
